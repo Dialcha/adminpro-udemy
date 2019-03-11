b612c413311f7d484a931ff7662a1a2b78adbda8
Diego <diego.chavarriap@gmail.com>
2019-03-11 14:52:40 -0500

    Fin de la sección 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
     BreadcrumbsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
